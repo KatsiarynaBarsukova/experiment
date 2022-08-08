ec312c500f6a0a1c7e59f44354e29f6f6b075593
KatsiarynaBarsukova <tulovo@tut.by>
2022-08-08 21:18:28 +0300

    мой коммит!

--- src/Hell.java
+++ src/Hell.java
@@ -1,5 +1,9 @@
 public class Hell {
     public static void main(String[] args) {
         System.out.println(111);
+        System.out.println("SomeNewCodeInNewBranch");
+        System.out.println("HelloGit!");
+        System.out.println("HelloGit!!!");
+
     }
 }
